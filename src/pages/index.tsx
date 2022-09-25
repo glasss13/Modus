d192aa031424f313bc789e0cf2a42b65abd127ca
Max Glass <glassmax13@gmail.com>
2022-09-25 18:36:25 -0400

    Make loading spinner while fetching classes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { trpc } from "../utils/trpc";
 import ClassCard from "../components/classCard";
 import CreateClass from "../components/createClass";
+import { Button } from "react-daisyui";
 
 const Home: NextPage = () => {
   const { data: classes } = trpc.useQuery(["class.getClasses"]);
@@ -18,7 +19,7 @@ const Home: NextPage = () => {
       <h1 className="text-center text-5xl md:text-[5rem] leading-normal font-extrabold">
         Modus Grade Calculator
       </h1>
-      <main className="container flex flex-col min-h-screen mx-auto p-1 w-2/3 border border-gray-600 ">
+      <main className="container flex flex-col min-h-screen mx-auto p-1 w-2/3  ">
         {classes ? (
           classes.map(class_ => (
             <ClassCard
@@ -28,7 +29,7 @@ const Home: NextPage = () => {
             />
           ))
         ) : (
-          <p>loading...</p>
+          <Button size="lg" color="ghost" loading />
         )}
         <CreateClass />
       </main>
