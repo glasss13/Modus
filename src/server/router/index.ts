8b31687a5f29972dec35bac53ec687987041c2f1
Max Glass <glassmax13@gmail.com>
2022-10-02 12:48:46 -0400

    Remove some more boilerplate

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -1,17 +1,13 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
-import { protectedExampleRouter } from "./protected-example-router";
 import { classRouter } from "./class";
 import { assignmentRouter } from "./assignment";
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge("class.", classRouter)
-  .merge("assignment.", assignmentRouter)
-  .merge("example.", exampleRouter)
-  .merge("auth.", protectedExampleRouter);
+  .merge("assignment.", assignmentRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
