a2e1f73a8bd573e86548c32d0770a0638cc7404c
Max Glass <glassmax13@gmail.com>
2022-10-02 17:41:54 -0400

    Add login and user status in top right

--- next.config.mjs
+++ next.config.mjs
@@ -20,4 +20,12 @@ export default defineNextConfig({
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 });
