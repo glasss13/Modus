f033aa433fec6a6f9a92592d96c21e63cacf8d55
Max Glass <glassmax13@gmail.com>
2023-02-24 11:46:21 -0500

    No longer server render homepage
    It was causing client and server state to get out of sync when mutating

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,9 @@
-import type {
-  GetServerSidePropsContext,
-  InferGetServerSidePropsType,
-  NextPage,
-} from "next";
+import type { GetServerSidePropsContext } from "next";
+import { Button } from "react-daisyui";
 import ClassCard from "../components/classCard";
 import CreateClass from "../components/createClass";
 import { getServerAuthSession } from "../server/common/get-server-auth-session";
-import { prisma } from "../server/db/client";
-import { fullClass } from "../server/router/class";
+import { trpc } from "../utils/trpc";
 
 export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
   const session = await getServerAuthSession(ctx);
@@ -18,43 +14,36 @@ export const getServerSideProps = async (ctx: GetServerSidePropsContext) => {
         destination: "/signIn",
         permanent: false,
       },
-      props: { classes: [] },
     };
 
-  const classes = await prisma.class.findMany({
-    where: {
-      userId: session.user.id,
-    },
-    include: fullClass,
-  });
-
   return {
-    props: { classes },
+    props: {},
   };
 };
 
-const Home: NextPage<
-  InferGetServerSidePropsType<typeof getServerSideProps>
-> = ({ classes }) => {
+export default function Home() {
+  const { data: classes, isLoading } = trpc.useQuery(["class.getClasses"]);
   return (
     <>
       <h1 className="text-center text-5xl font-extrabold leading-normal md:text-[5rem]">
         Modus
       </h1>
       <main className="container mx-auto flex min-h-screen w-2/3 flex-col p-1  ">
-        {/* {isError && error.data?.code === "UNAUTHORIZED" ? } */}
-        {classes.map(class_ => (
-          <ClassCard
-            key={class_.id}
-            id={class_.id}
-            name={class_.name}
-            standards={class_.standards}
-          />
-        ))}
+        {}
+        {classes ? (
+          classes.map(class_ => (
+            <ClassCard
+              key={class_.id}
+              id={class_.id}
+              name={class_.name}
+              standards={class_.standards}
+            />
+          ))
+        ) : isLoading ? (
+          <Button size="lg" color="ghost" loading />
+        ) : null}
         <CreateClass />
       </main>
     </>
   );
-};
-
-export default Home;
+}
